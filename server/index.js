e51bb7eefdec2b0b65b6c6fb3253be7f3223f376
emre.yenigun <emre.yenigun@borngroup.com>
2021-01-25 20:47:53 -0500

    API: Succesful implementation of api fetchware

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const PORT = 5500;
 const morgan = require('morgan');
-const http = require('http');
+const http = require('https');
 const server = app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
@@ -19,44 +19,80 @@ app.use(morgan('dev'));
  */
 
 /**
- * This function sends an http request to the foodish API and receives an object with an image path
+ * This function sends an http request to the foodish API and returns a Promise
  */
 
-const generatePhoto = async () => {
-    try {
-        http.request(
-            {
-                hostname: 'foodish-api.herokuapp.com',
-                path: '/api/',
-            },
-            (res) => {
-                let data = '';
-
-                res.on('data', (d) => {
-                    data += d;
-                });
-                res.on('end', () => {
-                    return data;
-                });
+const generatePhoto = () => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: 'foodish-api.herokuapp.com',
+            path: '/api/',
+            method: 'GET',
+        };
+        const req = http.request(options, (res) => {
+            if (req.statusCode < 200 || res.statusCode >= 300) {
+                return reject(new Error('statusCode=', res.statusCode));
             }
-        ).end();
-    } catch (error) {
-        console.log(error);
-    }
+            let body = [];
+            res.on('data', (d) => {
+                body.push(d);
+            });
+            res.on('end', () => {
+                try {
+                    body = JSON.parse(Buffer.concat(body).toString());
+                } catch (e) {
+                    reject(e);
+                }
+                resolve(body);
+            });
+        });
+        req.on('error', (e) => {
+            reject(e.message);
+        });
+        req.end();
+        return req;
+    });
 };
+
+generatePhoto().then((data) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    };
+    return response;
+});
 
 /**
  * By calling this function from the browser, a parameter n will generate the user's
  * desired number of images and return an array of these images
  */
-const generatePics = (n) => {
+const generatePics = async (n) => {
     let pics = [];
     for (let i = 0; i < n; i++) {
-        let datapoint = generatePhoto();
+        let datapoint = await generatePhoto().then((data) => {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify(data),
+            };
+            return response;
+        });
         pics.push(datapoint);
     }
     return pics;
 };
+
+/**
+ * This route calls the api based on the input value and sends back the photo array
+ */
+app.use('/foodpics/:n', async (req, res) => {
+    try {
+        const input = req.params.n;
+        const results = await generatePics(input);
+        res.send(results);
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 /**
  * Static root route to serve up HTML
@@ -68,19 +104,4 @@ app.use('/', async (req, res) => {
         console.log(err);
     }
 });
-
-/**
- * This route calls the api based on the input value and sends back the photo array
- */
-app.use('/foodpics/:n', async (req, res) => {
-    try {
-        const input = req.params.n;
-        const results = generatePics(input);
-        console.log(results);
-        res.send(results);
-    } catch (err) {
-        console.log(err);
-    }
-});
-
 module.exports = app;
