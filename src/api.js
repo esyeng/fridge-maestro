91b52f68711d72ebc1455109fce81697be9623cb
emre.yenigun <emre.yenigun@borngroup.com>
2021-01-29 22:07:02 -0500

    JS: need some help

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const api = (function () {
+    return {
+        baseUrl: `https://api.spoonacular.com/`,
+        ingredientSearch: `https://api.spoonacular.com/food/ingredients/search`,
+        recipeSearch: `GET https://api.spoonacular.com/recipes/complexSearch`,
+        ingredientInfo: (id) =>
+            `https://api.spoonacular.com/food/ingredients/${id}/information`,
+        random: `https://api.spoonacular.com/recipes/random`,
+    };
+    // recipeByIngredients: `https://api.spoonacular.com/recipes/findByIngredients`,
+})();
