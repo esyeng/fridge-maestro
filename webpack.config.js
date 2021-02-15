a7ea80525a6b4c62fe29469490f93f64f89d0f72
Emre Yenigun <emre.yenigun@borngroup.com>
2021-02-15 17:46:28 -0500

    Config deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,3 @@
-// const path = require('path');
-
-// module.exports = {
-//     mode: 'production',
-//     entry: './src/index.js',
-//     output: {
-//         filename: 'main.js',
-//         path: path.resolve(__dirname, 'dist'),
-//     },
-// };
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
