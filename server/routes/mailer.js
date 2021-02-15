c949663d03573c17d23c46b6587016f0bbb0c5f2
Emre Yenigun <emre.yenigun@borngroup.com>
2021-02-15 17:18:41 -0500

    Configures mail client

--- server/routes/mailer.js
+++ server/routes/mailer.js
@@ -3,20 +3,116 @@
 const express = require('express');
 const mailer = express.Router();
 const nodemailer = require('nodemailer');
-const json2html = require('node-json2html');
+require('dotenv').config();
+/**
+ * reference object
+ * {
+  recipient: 'yenigun13@gmail.com',
+  recipes: [
+    { id: 604816, data: [Object], instructions: [Array] },
+    { id: 523194, data: [Object], instructions: [Array] }
+  ]
+}
+* reference output {
+     <div>
+    <h1>Orange- Glazed Grilled Shrimp</h1>
+    <img src='https://spoonacular.com/recipeImages/491525-312x231.jpg' >
+        <h3>Ingredients:</h3>
+            <p>Missing: </p>
+            <ul><li>1/1/2 tsp. grainy mustard</li><li>1/4 cup orange marmalade</li></ul>
+            <p>Used:</p>
+            <ul><li>16 large shrimp (with tails on)</li></ul>
+            <p>Unused: </p>
+            <ul></ul>
+            <p>Instructions: </p>
+            <ol><li>No instructions found</li></ol>
+    </div>
+}
+*
+*
+ */
 
 module.exports = mailer;
-//payload === [recipe: id: 0, data: {...} instructions: [...], recipe]
 
-function htmlFromJson(payload) {
-    let template = { '<>': 'div', html: '${piece} ${value}' };
-    let data = [
-        { piece: 'title', value: `${payload.data.title}` },
-        { piece: 'body', value: `${payload.instructions}` },
-    ];
+function listProcessing(list) {
+    let toList = ``;
+    if (list && list.length > 0) {
+        for (let i = 0; i < list.length; i++) {
+            let item = list[i];
+            toList += `<li>${item.original}</li>`;
+        }
+    }
+    let result = `<ul>${toList}</ul>`;
+    return result;
+}
 
-    let html = json2html.transform(data, template);
-    return html;
+function instructionProcessing(recipe) {
+    let toInst = ``;
+    let list = recipe.instructions;
+    if (list[0] && list[0].steps && list[0].steps.length > 0) {
+        for (let i = 0; i < list[0].steps.length; i++) {
+            let inst = list[0].steps[i];
+            toInst += `<li>${inst.number}: ${inst.step}</li>`;
+        }
+    } else {
+        toInst += `<li>No instructions found</li>`;
+    }
+    let result = `<ul>${toInst}</ul>`;
+    return result;
+}
+
+function assembleBody(recipe) {
+    const used = listProcessing(recipe.data.usedIngredients);
+    const unused = listProcessing(recipe.data.unusedIngredients);
+    const missing = listProcessing(recipe.data.missedIngredients);
+    const instructions = instructionProcessing(recipe);
+    console.log({
+        missing: missing,
+        used: used,
+        unused: unused,
+        instructions: instructions,
+    });
+    return `
+    <div>
+    <h1>${recipe.data.title}</h1>
+    <img src='${recipe.data.image}' >
+        <h3>Ingredients:</h3>
+            <p>Missing: </p>
+            ${missing}
+            <p>Used:</p>
+            ${used}
+            <p>Unused: </p>
+            ${unused}
+            <p>Instructions: </p>
+            ${instructions}
+    </div>`;
+}
+
+function htmlify(arrRecipes) {
+    let bodyText = ``;
+
+    for (let i = 0; i < arrRecipes.length; i++) {
+        let recipe = arrRecipes[i];
+        bodyText += assembleBody(recipe);
+    }
+
+    console.log(bodyText);
+    let result = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+            <body style='background-color: black; color: white; font-family: sans-serif;' >
+            <div>
+                 ${bodyText}
+            </div>
+
+            </body>
+        </html>
+        `;
+    return result;
 }
 
 function genMessage(from, to, subject, text, html) {
@@ -38,43 +134,38 @@ function stringifyPayload(payload) {
 }
 
 async function mail(recipient, payload) {
-    let acct = recipient ? sendTo : await nodemailer.createTestAccount();
-
-    const transport = nodemailer.createTransport({
-        host: 'smtp.mailtrap.io',
-        port: 2525,
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
         auth: {
-            user: '53072868b86f27',
-            pass: '13ab555a182a0c',
+            user: 'fridgemaestro@gmail.com',
+            pass: process.env.GMAIL_TOKEN,
         },
     });
 
     // send mail with defined transport object
     let payloadText = stringifyPayload(payload);
-    let payloadHTML = htmlFromJson();
+    let payloadHTML = htmlify(payload);
     let message = genMessage(
         'Fridge Maestro <fm@fridgemaestro.com>',
         recipient,
-        "Here's a sheet, now make music!",
+        'Here are your notes, now make that fridge sing!',
         payloadText,
         payloadHTML
     );
 
-    // let info = await transport.sendMail({
-    //     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    //     to: 'bar@example.com, baz@example.com', // list of receivers
-    //     subject: 'Hello ✔', // Subject line
-    //     text: 'Hello world?', // plain text body
-    //     html: '<b>Hello world?</b>', // html body
-    // });
+    transporter.sendMail(message, function (err, info) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
 
 mailer.post('/', async (req, res, next) => {
     try {
-        console.log(req.body);
-        const { recipient, recipes } = req.body;
-
-        res.json({ msg: 'Mail received, thanks friend :|', why: 'tryouts' });
+        await mail(req.body.recipient, req.body.recipes);
+        res.json({ msg: 'Data received, mailing attempted' });
         // }
     } catch (err) {
         next(err);
